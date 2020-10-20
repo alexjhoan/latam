c2134f467a041178365791426518e789e8ec8289
pablo-novoa <novoanpablo@gmail.com>
2020-10-20 10:34:58 -0300

    edit

--- styles/carousel/slick-carousel.js
+++
@@ -1,7 +0,0 @@
-// css externo
-import css from 'styled-jsx/css'
-
-export default css.global`
-  @import url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css');
-  @import url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css');
-`
--- styles/font.js
+++
@@ -1,6 +0,0 @@
-// css externo
-import css from 'styled-jsx/css'
-
-export default css.global`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap');
-`
