c6aeb43f8522cd1af06f92b6cb7fa3da567ee43b
alexjhoan <alexjhoan.25@gmail.com>
2020-11-05 14:56:12 -0400

    new link talks 1500 05112020

--- components/Talks.tsx
+++ components/Talks.tsx
@@ -24,6 +24,11 @@ const dataVideo = [
 		title: 'Bienvenida LatAm Invierte',
 		subTitle: '¿Cómo funciona?',
 		idVideo: 'iZzlcrVRPxo'
+	},
+	{
+		title: 'Bienvenida LatAm Invierte',
+		subTitle: '¿Cómo funciona?',
+		idVideo: '-DRy5zD4T2o'
 	}
 ]
 
@@ -45,7 +50,7 @@ export default function Talks() {
 
   const paramsSwiper = {
     spaceBetween: 10,
-    loop: false,
+    loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
