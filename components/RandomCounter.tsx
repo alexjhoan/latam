059206c4e0412aea82524f77191c3c3cf2261849
pablo-novoa <novoanpablo@gmail.com>
2020-11-05 10:58:18 -0300

    edit

--- components/RandomCounter.tsx
+++ components/RandomCounter.tsx
@@ -20,7 +20,7 @@ export const RandomCounter = ({min, max}:RandomCounterProps) => {
             setTimeout(() => {
                 let variation = randomNumber(1, 5)
                 let prob = randomNumber(1, 3)
-                console.log('change counters')
+
                 if (prob === 1) {
                     if ((counter < 1000) && (counter > (min-1) )) {
                         setCounter( counter => counter - variation)
